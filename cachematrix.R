61d4d64356742dbc8ce54ee238ff826cc67f8b4b
certiprince <certaintyprinciple@outlook.com>
2020-04-16 14:35:32 +0100

    Added 2 required functions makeCacheMatrix() and cacheSolve()

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix takes in a single matrix argument.
+## x must be an invertible matrix.
+## It creates a matrix object and can store the inverse of matrix in cache.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        ## set() assigns new matrix y to replace x in parent environment.
+        ## This indicates that the inverse matrix 'inv' of x which exists in the cache
+        ## must be deleted. Hence it sets inv to NULL in parent environment.
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        ## get() returns the matrix x
+        get <- function() x
+        ## setinv() sets inv to the inverse of matrix x
+        setinv <- function(solve) inv <<- solve
+        ## getinv() returns inv
+        getinv <- function() inv
+        
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes in the matrix object from makeCacheMatrix. It checks if
+## inverse has already been calculated. If it exists, it returns inv from cache.
+## If no cached inv, it calculates inv and sets it in makeCacheMatrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        ## check for inverse. If exists, returns cached inv
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinv(inv)
+        inv
 }
